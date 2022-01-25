6e0c40ea1806ebff2daf702bb7014e7018567b3c
degra <degra@192.168.1.66>
2022-01-25 14:53:02 +0100

    oui

---
+++ web2/src/main/java/fr/formation/inti/controller/Connexion.java
@@ -0,0 +1,78 @@
+package fr.formation.inti.controller;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServelet
+ */
+
+public class Connexion extends HttpServlet {
+
+	private String color;
+	
+	private static final long serialVersionUID = 1L;
+       
+    public Connexion() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Writer out = response.getWriter();
+		out.append("<html>")
+		.append("<head><title>Connexion</title></head>")
+		.append("<body>")
+		
+		.append("<form action=\"Connexion\" method=\"post\">"
+				
+				+ "<label style=\"color:"+color+"\" for=\"Login\">Login:</label><br>"
+				+ "<input type=\"text\" id=\"Login\" name=\"Login\"><br>"
+				
+				+ "<label style=\"color:"+color+"\" for=\"PassWord\">Password:</label><br>\r\n"
+				+ "<input  type=\"password\" id=\"PassWord\" name=\"PassWord\"><br><br>"
+				
+				+ "<input type=\"submit\" value=\"Submit\">"
+				
+				+ "</form>")
+		
+		.append("<form action=\"index.html\" method=\"post\">"
+	
+				
+				+ "<input type=\"submit\" value=\"Retour home\">"
+				
+				+ "</form>")
+		.append("</body>")
+		.append("</html>");
+
+	}
+		
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String login = request.getParameter("Login");
+		String password = request.getParameter("PassWord");
+		color ="black";
+		if(("root".equals(login)) && ("123456".equals(password))){
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/LoginServlet");
+			dispatcher.forward(request, response);
+		}else {
+			color = "red";
+			doGet(request,response);
+		}
+
+	}
+
+}
---
+++ web2/src/main/java/fr/formation/inti/controller/LoginServlet.java
@@ -0,0 +1,60 @@
+package fr.formation.inti.controller;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Servlet implementation class LoginServelet
+ */
+
+public class LoginServlet extends HttpServlet {
+	private static final Log log = LogFactory.getLog(LoginServlet.class);
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		log.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>doPost LoginServlet");
+		String login = request.getParameter("Login");
+		
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        String date = dtf.format(LocalDateTime.now()).toString();
+
+		ServletOutputStream out = response.getOutputStream();
+		out.println("<html>");
+		out.println("<head><title>LoginServlet</title></head>");
+		out.println("<body>");
+		out.println("<p>hello "+login+"</p>");
+		out.println("<p>you are connected</p>");
+		out.println("<p>"+date+"</p>");
+		
+		out.println("<form action=\"index.html\" method=\"post\">"		
+				+ "<input type=\"submit\" value=\"Retour home\">"		
+				+ "</form>");
+
+}
+}
